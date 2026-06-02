5413f05bb0461838e9d87eb43e19a293fb5f9a33
R1ck307 <boylilritchie@gmail.com>
2026-06-02 21:34:18 +0200

    Delete rules.py

--- rules.py
+++
@@ -1,9 +0,0 @@
-def diagnose(crop, symptoms):
-
-    if crop == "Maize" and "yellow_leaves" in symptoms:
-        return "Nitrogen Deficiency"
-
-    if crop == "Tomato" and "brown_spots" in symptoms:
-        return "Early Blight"
-
-    return None
